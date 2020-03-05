a67daed0b0d0e3ed7c6eca3086a57bb76162855f
jod35 <jod35@gmail.com>
2020-03-05 13:52:37 -0600

    added postgresql database

--- simple_blog/__init__.py
+++ simple_blog/__init__.py
@@ -1,25 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from .config import Config
 from flask_migrate import Migrate
+from .config import Config
 from flask_admin import Admin
-
 
 app=Flask(__name__)
 app.config.from_object(Config)
-
-
-
 db=SQLAlchemy(app)
 migrate=Migrate(app,db)
-
-login_manager=LoginManager()
-login_manager.init_app(app)
-
-login_manager.login_view='login'
-login_manager.login_message='Login to access this page'
-admin=Admin(app,name="Fun blog",template_mode='bootstrap3')
-
+login_manager=LoginManager(app)
+admin=Admin(app)
 
 from . import views
+
--- simple_blog/config.py
+++ simple_blog/config.py
@@ -1,7 +1,7 @@
 class Config:
     my_pw="nathanoj35"
     SQLALCHEMY_TRACK_MODIFICATIONS=False
-    SQLALCHEMY_DATABASE_URI='mysql://jon:{}@localhost/simple'.format(my_pw)
+    SQLALCHEMY_DATABASE_URI='postgresql://jona:{}@localhost/blog'.format(my_pw)
     SQLALCHEMY_ECHO=False
     SECRET_KEY='0043e3594ceb57e473b15e0b2c74e035e45a018533caddd8654a20b92a0f8f31'
     DEBUG=True
